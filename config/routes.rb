6dba423030ef08521ba962f72f033c2cca3b8b15
cheng.shi <cheng.shi@appcara.com>
2016-04-08 16:57:37 +0800

    add asset resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  #get 'location/index'
-  root to: 'location#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: 'location#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
